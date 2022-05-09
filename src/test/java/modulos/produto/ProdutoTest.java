17bea7bfe8221ba57659e2a819dd713905d699da
Your Name <you@example.com>
2022-05-09 11:52:02 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -0,0 +1,66 @@
+package modulos.produto;
+
+import dataFactory.ProdutoDataFactory;
+import dataFactory.UsuarioDataFactory;
+import io.restassured.http.ContentType;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+import pojo.UsuarioPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Testes de API Rest do modulo de Produto")
+public class ProdutoTest {
+    private String token;
+
+    @BeforeEach
+    public void beforeEach(){
+        // Configurando os dados da API Rest da Lojinha
+        baseURI = "http://165.227.93.41";
+        //port = 8080;
+        basePath = "/lojinha";
+
+
+
+        // Obter o token do usuario admin
+        this.token = given()
+                .contentType(ContentType.JSON)
+                .body(UsuarioDataFactory.criarUsuarioAdmnistrador())
+            .when()
+                .post("/v2/login")
+            .then()
+                .extract()
+                    .path("data.token");
+    }
+
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do Produto")
+    public void testValidarLimitesProibidosValorProduto(){
+
+        // Tentar inserir o produto com valo 0.00 e validar que a mensagem de erro foi apresentada e o
+        // status code retornado foi 422
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body(ProdutoDataFactory.criarProdutoComumComValorIgualA(0.00));
+        when()
+                .post("/v2/produtos");
+        then()
+                .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+    }
+
+    private MatcherAssert then() {
+    }
+}
